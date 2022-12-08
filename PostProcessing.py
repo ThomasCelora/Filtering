ad2331ea5eaa2d2dd9e28ce4ec6288e4880827d4
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2022-12-08 12:59:08 +0000

    combined work from home and office

--- PostProcessing.py
+++ PostProcessing.py
@@ -26,9 +26,9 @@ class PostProcessing(object):
         fs1 = []
         num_files = 11
         for n in range(num_files):
-          fs1.append(h5py.File('./Data/KH/Ideal/dp_800x800x0_'+str(n)+'.hdf5','r'))
+          fs1.append(h5py.File('../Data/KH/Ideal/dp_400x800x0_'+str(n)+'.hdf5','r'))
         fss = [fs1]
-        nx = ny = 800
+        nx, ny = 400, 800
         
         ts = np.linspace(0,30,11) # Need to actually get these
         xs = np.linspace(-0.5,0.5,nx)
@@ -42,12 +42,18 @@ class PostProcessing(object):
         self.ns = []
         self.rhos = []
         self.ps = []
+        self.Ws = []
         for fs in fss[0]:
             self.vxs.append(fs['Primitive/v1'][:])
             self.vys.append(fs['Primitive/v2'][:])
             self.ns.append(fs['Primitive/n'][:])
             self.rhos.append(fs['Primitive/rho'][:])
             self.ps.append(fs['Primitive/p'][:])
+            self.Ws.append(fs['Auxiliary/W'][:])
+
+        self.ut = self.Ws
+        self.ux = np.dot(Ws,vxs])
+        self.uy = np.dot(Ws,vys])
         
         # EoS & dissipation parameters
         self.coefficients = {'gamma': 5/3,
@@ -57,23 +63,32 @@ class PostProcessing(object):
         
         self.dT = 0.01 # steps to take for differential calculations
         self.dX = 0.01
-        
+        self.dY = 0.01
+        self.cen_SO_stencil = [1/12, -2/3, 0, 2/3, -1/12]
+
         # Define Minkowski metric
         self.metric = np.zeros((4,4))
         self.metric[0,0] = -1
-        self.metric[1,1] = self.metric[2,2] = self.metric[3,3] = +1
+        self.metric[1,1] = metric[2,2] = metric[3,3] = +1
         
         # Load the coordinates and observers already calculated
         with open('KH_observers.pickle', 'rb') as f:
             # The protocol version used is detected automatically, so we do not
             # have to specify it.
             self.coord_list, self.vectors, self.funs = pickle.load(f)
+     
+    def calc_4vel(W,vx,vy):
+        return [W,W]
         
-    # def calc_NonId_terms(u,p,T):
-    #     Pi = -coefficients['zeta']*
-    #     q = coefficients['kappa']*
-    #     pi = -coefficients['eta']*()
-    #     return Pi, q, pi
+    def calc_NonId_terms(u,p,T):
+        # u = np.dot(W,[1,vx,vy]) # check this works...
+        dtut = calc_t_deriv(u, point)
+        Theta = calc_t_deriv(u[0], point) + calc_x_deriv(u[1],point) + calc_y_deriv(u[2],point)
+        Theta = dtut + dxux + dyuy + dzuz
+        Pi = -coefficients['zeta']*Theta
+        q = coefficients['kappa']*
+        pi = -coefficients['eta']*(dxuy + dyux - (2/3)*Theta)
+        return Pi, q, pi
     
     def p_from_EoS(rho, n):
         p = (self.coefficients['gamma']-1)*(rho-n)
@@ -89,38 +104,23 @@ class PostProcessing(object):
         NonId_SET = rho*u_mu_u_nu + (p+Pi)*h_mu_nu + np.outer(q,u) + np.outer(u,q) + pi
         return NonId_SET
     
-    def calc_t_deriv(quant, point):
+    def calc_t_deriv(quant_str, point):
         t, x, y = point
-        # values = [interpolate_quant(,,[ts,x,y]) for t in np.linspace(t-2*self.dT,t+2*self.dT,5)]
-        stencil = [1/12, -2/3, 0, 2/3, -1/12]
-        dt_quant = np.dot(stencil, values) / self.dT
+        values = [scalar_val(T,x,y,quant_str) for T in np.linspace(t-2*self.dT,t+2*self.dT,5)]
+        dt_quant = np.dot(self.cen_SO_stencil, values) / self.dT
         return dt_quant
     
-    def calc_X_deriv(quant):
-        dX_quant = np.dot(stencil, values) / self.dX
+    def calc_x_deriv(quant_str):
+        t, x, y = point
+        values = [scalar_val(t,X,y,quant_str) for X in np.linspace(x-2*self.dX,x+2*self.dX,5)]
+        dX_quant = np.dot(self.cen_SO_stencil, values) / self.dX
         return dX_quant
-    
-    def surface_flux(self, x,E_x,E_y,P,direc_vec):
-        point = P + x*(E_x + E_y)
-        u, n = self.interpolate_u_n_point(point)
-        n_mu = np.multiply(u,n)
-        return self.Mink_dot(n_mu,direc_vec)
-    
-    def residual_ib(self, V0_V1,P,L):
-        U = self.get_U_mu(V0_V1)
-        # U = get_U_mu_MagTheta(V0_V1)
-        E_x, E_y = self.construct_tetrad(U)
-        flux = 0
-        for i in range(3,-1,-1):
-            if i == 3:
-                flux += quad(func=self.surface_flux,a=-L/2,b=L/2,args=(E_x,E_y,P,-E_x))[0]
-            elif i == 2:
-                flux += quad(func=self.surface_flux,a=-L/2,b=L/2,args=(E_x,-E_y,P,-E_y))[0]
-            elif i == 1:
-                flux += quad(func=self.surface_flux,a=-L/2,b=L/2,args=(-E_x,-E_y,P,E_x))[0]
-            elif i == 0:
-                flux += quad(func=self.surface_flux,a=-L/2,b=L/2,args=(-E_x,E_y,P,E_y))[0]
-            return abs(flux)
+
+    def calc_y_deriv(quant_str):
+        t, x, y = point
+        values = [scalar_val(t,x,Y,quant_str) for Y in np.linspace(y-2*self.dX,y+2*self.dY,5)]
+        dX_quant = np.dot(self.cen_SO_stencil, values) / self.dY
+        return dX_quant
     
     def scalar_val(self, t, x, y, quant_str):
         return interpn(self.points,self.vars[quant_str],[t,x,y])
@@ -129,11 +129,13 @@ class PostProcessing(object):
         # contruct tetrad...
         E_x, E_y = Base.construct_tetrad(U)
         corners = Base.find_boundary_pts(E_x,E_y,P,L)
+        start, end = corners[0], corners[2]
         t, x, y = point
-        integrated_quant = tplquad(self.scalar_val,t-(L/2),t+(L/2),x-(L/2),x+(L/2),y-(L/2),y+(L/2),args=quant_str)
+        # integrated_quant = nquad(self.scalar_val,t-(L/2),t+(L/2),x-(L/2),x+(L/2),y-(L/2),y+(L/2),args=quant_str)
+        integrated_quant = nquad(self.scalar_val,[[start[0],end[0]],[start[1],end[1]],start[2],end[2]],args=quant_str)
         return integrated_quant / (L**3) # seems too simple!?
 
-    def project_tensor(self, vector1_wrt, vector2_wrt, to_project):
+    def project_tensor(vector1_wrt, vector2_wrt, to_project):
         projection = np.inner(vector1_wrt,np.inner(vector2_wrt,to_project))
         return projection
     
