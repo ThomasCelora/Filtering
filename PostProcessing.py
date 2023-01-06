f73aaf29f5528650197f8f1a152d34834aaa70de
MarcusHatton <74266732+MarcusHatton@users.noreply.github.com>
2023-01-06 15:24:53 +0000

    Getting more there...

--- PostProcessing.py
+++ PostProcessing.py
@@ -17,7 +17,7 @@ from scipy.optimize import root, minimize
 from scipy.integrate import solve_ivp, quad, tplquad, nquad
 import cProfile, pstats, io
 from system.BaseFunctionality import Base
-
+import math
 
 class PostProcessing(object):
         
@@ -27,16 +27,16 @@ class PostProcessing(object):
         for n in range(num_files):
           fs1.append(h5py.File('./Data/KH/Ideal/dp_400x400x0_'+str(n)+'.hdf5','r'))
         fss = [fs1]
-        nx, ny = int(400), int(400)
-        c_nx, c_ny = int(nx/2), int(ny/2) # coarse
+        self.nx, self.ny = int(400), int(400)
+        self.c_nx, self.c_ny = int(self.nx/2), int(self.ny/2) # coarse
         
-        ts = np.linspace(0,30,11) # Need to actually get these
-        xs = np.linspace(-0.5,0.5,nx)
-        ys =  np.linspace(-1.0,1.0,ny)
-        self.points = (ts,xs,ys)
+        self.ts = np.linspace(0,30,11) # Need to actually get these
+        self.xs = np.linspace(-0.5,0.5,self.nx)
+        self.ys =  np.linspace(-1.0,1.0,self.ny)
+        self.points = (self.ts,self.xs,self.ys)
         # self.dt get this...
-        self.dx = (xs[-1] - xs[0])/nx # actual grid-resolution
-        self.dy = (ys[-1] - ys[0])/ny
+        self.dx = (self.xs[-1] - self.xs[0])/self.nx # actual grid-resolution
+        self.dy = (self.ys[-1] - self.ys[0])/self.ny
         self.vxs = []
         self.vys = []
         self.ns = []
@@ -53,9 +53,9 @@ class PostProcessing(object):
             self.Ws.append(fs['Auxiliary/W'][:])
             self.Ts.append(fs['Auxiliary/T'][:])
             vxs_fine = fs['Primitive/v1'][:]
-            vxs_coarse = np.zeros((c_nx,c_ny))
-            for i in range(c_nx):
-                for j in range(c_ny):
+            vxs_coarse = np.zeros((self.c_nx,self.c_ny))
+            for i in range(self.c_nx):
+                for j in range(self.c_ny):
                     vxs_coarse[i][j] = vxs_fine[i*2][j*2] + vxs_fine[i*2+1][j*2] \
                                        + vxs_fine[i*2][j*2+1] + vxs_fine[i*2][j*2+1]
 
@@ -66,7 +66,7 @@ class PostProcessing(object):
         self.vars = {'v_x': self.vxs,
                           'v_y': self.vys,
                           'n': self.ns,
-                          'rho': self.rhos,
+                          'R': self.rhos,
                           'p': self.ps,
                           'W': self.Ws,
                           'u_t': self.ut,
@@ -98,12 +98,11 @@ class PostProcessing(object):
             # have to specify it.
             self.coord_list, self.vectors, self.funs = pickle.load(f)
      
-    def calc_4vel(W,vx,vy):
-        return [W,W]
+    # def calc_4vel(W,vx,vy):
+    #     return [W,W]
         
     def calc_NonId_terms(self,u,p,rho,n):
         # u = np.dot(W,[1,vx,vy]) # check this works...
-        T = p/n
         dtut = self.calc_t_deriv('u_t',point)
         dxux = self.calc_x_deriv('u_x',point)
         dyuy = self.calc_y_deriv('u_y',point)
@@ -163,12 +162,12 @@ class PostProcessing(object):
         start, end = corners[0], corners[2]
         t, x, y = point
         # integrated_quant = nquad(self.scalar_val,t-(L/2),t+(L/2),x-(L/2),x+(L/2),y-(L/2),y+(L/2),args=quant_str)
+        print(type(quant_str))
         print(start,end)
         print(start[0],end[2])
-        print(quant_str)
-        # integrated_quant = nquad(func=self.scalar_val,ranges=[[start[0],end[0]],[start[1],end[1]],[start[2],end[2]]],args=quant_str)
-        integrated_quant = nquad(func=self.scalar_val_point,
-            ranges=[[start[0], start[1],start[2]],[end[0],end[1],end[2]]],args=quant_str)
+        integrated_quant = nquad(func=self.scalar_val,ranges=[[start[0],end[0]],[start[1],end[1]],[start[2],end[2]]],args=quant_str)
+        # integrated_quant = nquad(func=self.scalar_val_point,
+        #     ranges=[[start[0], start[1],start[2]],[end[0],end[1],end[2]]],args=quant_str)
         return integrated_quant[0] / (L**3) # seems too simple!?
 
     def project_tensor(vector1_wrt, vector2_wrt, to_project):
@@ -179,11 +178,24 @@ class PostProcessing(object):
         return self.metric + np.outer(u,u)
     
     def values_from_hdf5(self, point, quant_str):
-        return self.macros[quant_str][point[0],point[1],point[2]] # fix
+        t_label, x_label, y_label = self.find_nearest_cell(point)
+        return self.vars[quant_str][t_label][x_label, y_label] # fix
+    
+    def find_nearest(self, array,value):
+        idx = np.searchsorted(array, value, side="left")
+        if idx > 0 and (idx == len(array) or math.fabs(value - array[idx-1]) < math.fabs(value - array[idx])):
+            return array[idx-1]
+        else:
+            return array[idx]
+        
+    def find_nearest_cell(self, point):
+        t_pos = self.find_nearest(self.ts,point[0])
+        x_pos = self.find_nearest(self.xs,point[1])
+        y_pos = self.find_nearest(self.xs,point[2])
+        print(t_pos,x_pos,y_pos)
+        return [np.where(self.ts==t_pos)[0][0], np.where(self.xs==x_pos)[0][0], np.where(self.ys==y_pos)[0][0]]
     
 if __name__ == '__main__':
-
-    import pickle
     
     Processor = PostProcessing()
     
@@ -205,7 +217,7 @@ if __name__ == '__main__':
 
     #print(points,Us)
 
-    scalar_strs = ['n', 'rho', 'p']
+    scalar_strs = ['n', 'R', 'p']
     vector_strs = ['W', 'u_x', 'u_y']
     micros = []
     L = 0.1
@@ -218,9 +230,12 @@ if __name__ == '__main__':
             # print(Ns)
 
         # Filter scalar fields
-        N, Rho, P = Processor.filter_scalar(point, U, scalar_strs[0], L),\
-            Processor.filter_scalar(point, U, scalar_strs[1], L), Processor.filter_scalar(point, U, scalar_strs[2], L)
+        N = Processor.filter_scalar(point, U, scalar_strs[0], L)
+        Rho = Processor.filter_scalar(point, U, scalar_strs[1], L)
+        P = Processor.filter_scalar(point, U, scalar_strs[2], L)
         T = P/N
+        
+        
 
         # Obtain coarse values
         n, rho, p = Processor.values_from_hdf5(point, scalar_strs[0]),\
