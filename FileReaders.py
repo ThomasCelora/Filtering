6d3c330eb5cad9e21736b5a987c3db7f2e9af756
Thomas Celora <thomas@uos-18598.lan>
2023-04-04 17:14:12 +0200

    TC 1st version of MicroModel + FileReader
    Committer: Thomas Celora <thomas.celora@gmail.com>
    On branch generalising
    Your branch is ahead of 'origin/generalising' by 1 commit.
       (use "git push" to publish your local commits)
    
    Changes to be committed:
    	deleted:    Data/Testing/data_serial_id_0.hdf5
    	deleted:    Data/Testing/data_serial_id_1.hdf5
    	deleted:    Data/Testing/data_serial_id_2.hdf5
    	deleted:    Data/Testing/data_serial_id_3.hdf5
    	deleted:    Data/Testing/data_serial_id_4.hdf5
    	deleted:    Data/Testing/data_serial_id_5.hdf5
    	new file:   Data/test_res100/res100_t0.hdf5
    	new file:   Data/test_res100/res100_t1.hdf5
    	new file:   Data/test_res100/res100_t2.hdf5
    	new file:   Data/test_res100/res100_t3.hdf5
    	new file:   Data/test_res100/res100_t4.hdf5
    	new file:   Data/test_res100/res100_t5.hdf5
    	modified:   FileReaders.py
    	modified:   MicroModels.py

--- FileReaders.py
+++ FileReaders.py
@@ -1,67 +1,91 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 27 17:58:02 2023
+Created on Fri Mar 31 10:00:00 2023
 
-@author: Marcus
+@author: Thomas
 """
 
 import h5py
-import glob 
+import glob
+import numpy as np
 
 class METHOD_HDF5(object):
 
     def __init__(self, directory):
-        hdf5_filenames = glob.glob(directory+str('ds*.hdf5'))
+        """
+        Set up the list of files (from hdf5) and strings for variables and domain
+
+        Parameters
+        ----------
+        directory: string 
+            the filenames in the directory have to be incremental (sorted is used)
+        """
+
+        hdf5_filenames = sorted( glob.glob(directory+str('*.hdf5')))
         self.hdf5_files = []
         for filename in hdf5_filenames:
             self.hdf5_files.append(h5py.File(filename,'r'))
         self.num_files = len(self.hdf5_files)
+
         self.key_strs = list(self.hdf5_files[0].keys())
         self.var_strs = []
         for key_str in self.key_strs:
             self.var_strs.append(list(self.hdf5_files[0][key_str].keys()))
+
             
+    def read_in_data(self,micro_model):   
+        """
+        Store data from files into micro_model 
+
+        Parameters
+        ----------
+        micro_model: class MicroModel 
+            strs in micromodel have to be the same as hdf5 files output from METHOD.
+        """ 
+        for prim_var_str in  micro_model.prim_vars:
+            for counter in range(self.num_files):
+                micro_model.prim_vars[prim_var_str].append( self.hdf5_files[counter]["Primitive/"+prim_var_str][:] )
+                # The [:] is for returning the arrays not the dataset
+            micro_model.prim_vars[prim_var_str]  = np.array(micro_model.prim_vars[prim_var_str])
         
-    def read_in_data(self, micro_model):
-        domain_const_strs = ['x','y']
-        # domain_const_strs = ['nt','nx','ny','Nx','Ny','x','y''dx','dy']
-        domain_var_strs = []# ['t']
-        prim_vars_strs = ['v1','v2','p','rho','n']
-        aux_vars_strs = ['W','T','h']
-        
-        # for domain_vars_str in micro_model.domain_vars:
-        for domain_vars_str in domain_const_strs:
-            micro_model.domain_vars[domain_vars_str] = self.hdf5_files[0]['Domain/'+domain_vars_str]
-        
-        print(self.num_files)
-        micro_model.nt = self.num_files
-        print(micro_model.nt)
-        micro_model.nx = len(self.hdf5_files[0]['Domain/x'][:])
-        micro_model.ny = len(self.hdf5_files[0]['Domain/y'][:])
 
-        for counter in range(self.num_files):
-            for prim_vars_str in micro_model.prim_vars_strs:
-                micro_model.prim_vars[prim_vars_str].append(self.hdf5_files[counter]['Primitive/'+prim_vars_str][:])
+        for aux_var_str in  micro_model.aux_vars:
+            for counter in range(self.num_files):
+                micro_model.aux_vars[aux_var_str].append( self.hdf5_files[counter]["Auxiliary/"+aux_var_str][:] )
+            micro_model.aux_vars[aux_var_str] = np.array(micro_model.aux_vars[aux_var_str])
+ 
 
-            for aux_vars_str in micro_model.aux_vars_strs:
-                micro_model.aux_vars[aux_vars_str].append(self.hdf5_files[counter]['Auxiliary/'+aux_vars_str][:])
+        # The following is temporary, as no extended info about domain are output by METHOD
+        # Can recover all info about spatial grid, not about times
 
+        micro_model.domain_vars["ts"] = np.arange(6)
+        micro_model.domain_vars["xs"] = np.array(self.hdf5_files[0]['Domain/'+"x"][:])
+        micro_model.domain_vars["ys"] = np.array(self.hdf5_files[0]['Domain/'+"y"][:])
 
+        micro_model.domain_vars["nt"] = self.num_files
+        micro_model.domain_vars["nx"] = len(micro_model.domain_vars["xs"])
+        micro_model.domain_vars["ny"] = len(micro_model.domain_vars["ys"])
 
+        micro_model.domain_vars["xmax"] = micro_model.domain_vars["xs"][-1]
+        micro_model.domain_vars["xmin"] = micro_model.domain_vars["xs"][0]
+        micro_model.domain_vars["ymax"] = micro_model.domain_vars["ys"][-1]
+        micro_model.domain_vars["ymin"] = micro_model.domain_vars["ys"][0]         
 
+        micro_model.domain_vars["dx"] = (micro_model.domain_vars["xmax"] - micro_model.domain_vars["xmin"]) \
+                                        / micro_model.domain_vars["nx"]
+        micro_model.domain_vars["dy"] = (micro_model.domain_vars["ymax"] - micro_model.domain_vars["ymin"]) \
+                                        / micro_model.domain_vars["ny"]
+
+        micro_model.domain_vars["points"] = [micro_model.domain_vars["ts"],micro_model.domain_vars["xs"],micro_model.domain_vars["ys"]]
+    
+    
 if __name__ == '__main__':
-    from MicroModels import *
 
-    FileReader = METHOD_HDF5('./Data/Testing/')
-    MicroModel = IdealHydro()
+    from MicroModels import * 
+
+    FileReader = METHOD_HDF5('./Data/test_res100/')
+    MicroModel = IdealMHD_2D()
     FileReader.read_in_data(MicroModel)
-    MicroModel.setup()
-    
-    
-    
-    
-    
-    
-    
+
     
     
--- MicroModels.py
+++ MicroModels.py
@@ -1,171 +1,231 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 27 18:13:07 2023
+Created on Fri Mar 31 10:00:02 2023
 
-@author: Marcus
+@author: Thomas
 """
 
 from FileReaders import *
+from scipy.interpolate import interpn 
 import numpy as np
-from scipy.interpolate import interpn
-from scipy.optimize import root, minimize
 
-class IdealHydro(object):
+# from scipy.optimize import root, minimize
 
-    def __init__(self):
-        self.nt = 0
-        self.nx = 0
-        self.ny = 0
-        self.Nx = 0
-        self.Ny = 0
-        self.xmin = 0
-        self.xmax = 0
-        self.ymin = 0
-        self.ymax = 0
-        self.xs = 0
-        self.ys = 0
-        self.dt = 0
-        self.dx = 0
-        self.dy = 0
-        self.ts = []
-        self.points = (self.ts,self.xs,self.ys)
-        
-        self.domain_vars = {'nt': self.nt,
-                            'nx': self.nx,
-                            'ny': self.ny,
-                            'Nx': self.Nx,
-                            'Ny': self.Ny,
-                            'xmin': self.xmin,
-                            'xmax': self.xmax,
-                            'ymin': self.ymin,
-                            'ymax': self.ymax,
-                            'x': self.xs,
-                            'y': self.ys,
-                            'dt': self.dt,
-                            'dx': self.dx,
-                            'dy': self.dy}
- 
-        # Define fluid variables for both the fine and coarse data
-        # self.vxs = np.zeros((self.nt, self.nx, self.ny))
-        # self.vys = np.zeros((self.nt, self.nx, self.ny))
-        # self.uts = np.zeros((self.nt, self.nx, self.ny))
-        # self.uxs = np.zeros((self.nt, self.nx, self.ny))
-        # self.uys = np.zeros((self.nt, self.nx, self.ny))
-        # self.ns = np.zeros((self.nt, self.nx, self.ny))
-        # self.rhos = np.zeros((self.nt, self.nx, self.ny))
-        # self.ps = np.zeros((self.nt, self.nx, self.ny))
-        # self.Ws = np.zeros((self.nt, self.nx, self.ny))
-        # self.Ts = np.zeros((self.nt, self.nx, self.ny))
-        # self.hs = np.zeros((self.nt, self.nx, self.ny))
-        # self.Id_SETs = np.zeros((self.nt, self.nx, self.ny, 3, 3))
-        
-        self.vxs = []
-        self.vys = []
-        self.ps = []
-        self.ns = []
-        self.rhos = []
+# These are the symbols, so be careful when using these to construct vectors!
+# levi3D = np.array([[[ np.sign(i-j) * np.sign(j- k) * np.sign(k-i) \
+#                       for k in range(3)]for j in range(3) ] for i in range(3) ])
 
-        self.prim_vars = {'v1': self.vxs,
-                          'v2': self.vys,
-                          'p': self.ps,
-                          'rho': self.rhos,
-                          'n': self.ns}
+# levi4D = np.array([[[[ np.sign(i - j) * np.sign(j - k) * np.sign(k - l) * np.sign(i - l) \
+#                        for l in range(4)] for k in range(4) ] for j in range(4)] for i in range(4)])
 
-        self.uts = []
-        self.uxs = []
-        self.uys = []
-        self.Id_SETs = []
-        
-        self.vars =       {'u_t': self.uts,
-                          'u_x': self.uxs,
-                          'u_y': self.uys,
-                          'Id_SET': self.Id_SETs}
 
-        self.Ws = []
-        self.Ts = []
-        self.hs = []
-        
-        self.aux_vars = {'W': self.Ws,
-                         'T': self.Ts,
-                         'h': self.hs}
+class IdealMHD_2D(object):
 
-        self.prim_vars_strs = ['v1','v2','p','rho','n']
-        self.var_strs = ['u_t','u_x','u_y']
-        self.aux_vars_strs= ['W','T','h']
+    def __init__(self, interp_method = "linear"):
+        """
+        Constructor, no parameters required. 
 
-        # Define Minkowski metric
+        Sets up the variables and dictionaries, strings correspond to 
+        those used in METHOD
+
+        Parameters
+        ----------
+        method: str
+            optional method to be used by interpn        
+        """
+        self.interp_method = interp_method
+
         self.metric = np.zeros((3,3))
         self.metric[0,0] = -1
         self.metric[1,1] = self.metric[2,2] = +1
 
-    def setup(self):
-        self.uts = self.Ws[:]
-        self.uxs = np.zeros((self.nt,self.nx,self.ny))
-        self.uys = np.zeros((self.nt,self.nx,self.ny))
-        self.Id_SETs = np.zeros((self.nt,self.nx,self.ny,3,3))
-        for h in range(self.nt):
-            for i in range(self.nx):
-                for j in range(self.ny):
-                    self.uts[h][i,j] = self.Ws[h][i,j]
-                    self.uxs[h][i,j] = self.Ws[h][i,j]*self.vxs[h][i,j]
-                    self.uys[h][i,j] = self.Ws[h][i,j]*self.vys[h][i,j]
-                    u_vec = np.array([self.uts[h][i,j],self.uxs[h][i,j],self.uys[h][i,j]])
-                    self.Id_SETs[h][i,j] = self.rhos[h][i,j]*np.outer(u_vec,u_vec)\
-                        + self.ps[h][i,j]*(self.metric + np.outer(u_vec,u_vec))
+        # This is the Levi-Civita symbol, not tensor, so be careful when using it 
+        self.Levi3D = np.array([[[ np.sign(i-j) * np.sign(j- k) * np.sign(k-i) \
+                      for k in range(3)]for j in range(3) ] for i in range(3) ])
 
-    def find_observer(self, point, residual):
-        """
-        Main function.
-        Finds the meso-observers, U, that the fluid has no drift with respect to.
+        #Dictionary for grid: info and points
+        self.domain_info_strs = ("nt","nx","ny","tmin","tmax","xmin","xmax","ymin","ymax","dt","dx","dy")
+        self.domain_points_strs = ("ts","xs","ys","points")
+        self.domain_vars = dict.fromkeys(self.domain_points_strs,self.domain_points_strs)
+        for str in self.domain_points_strs:
+            self.domain_points_vars = []   
+
+        #Dictionary for primitive var
+        self.prim_strs = ("vx","vy","rho","p","Bx","By")
+        self.prim_vars = dict.fromkeys(self.prim_strs)
+        for str in self.prim_strs:
+            self.prim_vars[str] = []
+
+        #Dictionary for auxiliary var
+        self.aux_strs = ("W","h","b0","bx","by","bsq")
+        self.aux_vars = dict.fromkeys(self.aux_strs)
+        for str in self.aux_strs:
+            self.aux_vars[str] = []
+
+        #Dictionary for structures
+        self.structures_strs = ("bar_vel","SET","Faraday")
+        self.structures = dict.fromkeys(self.structures_strs)
+        for str in self.structures_strs:
+            self.structures[str] = []
+
+    def get_domain_strs(self):
+        return self.domain_info_strs + self.domain_points_strs
     
+    def get_prim_strs(self):
+        return self.prim_strs
+    
+    def get_aux_strs(self):
+        return self.aux_strs
+    
+    def get_structures_strs(self):
+        return self.get_structures_strs
+
+    def setup_structures(self):
+        """
+        Set up the structures (i.e baryon vel, SET and Faraday) 
+
+        Structures are built as multi-dim np.arrays, with the first indices referring 
+        corrensponding to their components, the last three to the grid coordinates.
+        """
+        self.structures["bar_vel"] = np.zeros((3,self.domain_vars['nt'],self.domain_vars['nx'],self.domain_vars['ny']))
+        self.structures["SET"] = np.zeros((3,3,self.domain_vars['nt'],self.domain_vars['nx'],self.domain_vars['ny']))
+        self.structures["Faraday"] = np.zeros((3,3,self.domain_vars['nt'],self.domain_vars['nx'],self.domain_vars['ny']))
+
+        # print(type(self.aux_vars['W']))
+
+        for h in range(self.domain_vars['nt']):
+            for i in range(self.domain_vars['nx']):
+                for j in range(self.domain_vars['ny']): 
+                    self.structures["bar_vel"][0,h,i,j] = self.aux_vars['W'][h,i,j] 
+                    self.structures["bar_vel"][1,h,i,j] = self.aux_vars['W'][h,i,j] * self.prim_vars['vx'][h,i,j]
+                    self.structures["bar_vel"][2,h,i,j] = self.aux_vars['W'][h,i,j] * self.prim_vars['vy'][h,i,j]
+                    vel = np.array(self.structures["bar_vel"][:,h,i,j])
+
+                    fibr_b = np.array([self.aux_vars['b0'][h,i,j],self.aux_vars['bx'][h,i,j],self.aux_vars['by'][h,i,j]])
+
+                    # self.SET[:,:,h,i,j] = (self.rhos[h,i,j] + self.ps[h,i,j] + self.b2s[h,i,j]) * np.outer( vel,vel) + \
+                    #              (self.ps[h,i,j] + self.b2s[h,i,j]/2) * self.metric - np.outer(fibr_b, fibr_b)
+                    
+                    self.structures["SET"][:,:,h,i,j] = (self.prim_vars["rho"][h,i,j] + self.prim_vars["p"][h,i,j] + self.aux_vars["bsq"][h,i,j]) * \
+                                                    np.outer( vel,vel) + (self.prim_vars["p"][h,i,j] + self.aux_vars["bsq"][h,i,j]/2) * self.metric \
+                                                    - np.outer(fibr_b, fibr_b)
+                    
+                    fol_vel_vec = np.zeros(3)
+                    fol_vel_vec[0]=+1
+                    fol_b_vec = np.array([self.aux_vars["b0"][h,i,j],self.aux_vars["bx"][h,i,j],self.aux_vars["by"][h,i,j]])
+                    fol_e_vec = np.tensordot( self.Levi3D, np.outer(vel,fol_b_vec), axes = ([1,2],[0,1]))
+
+                    self.structures['Faraday'][:,:,h,i,j] = np.outer( fol_vel_vec,fol_e_vec) - np.outer(fol_e_vec,fol_vel_vec) -\
+                                                np.tensordot(self.Levi3D,fol_b_vec,axes=([2],[0]))
+
+
+    def get_interpol_prim(self, vars, point): 
+        """
+        Returns the interpolated variable at the point
+
         Parameters
         ----------
-        t_range, x_range, y_range : lists of 2 floats
-            Define the coordinate ranges of the points to find observers at.
-        L : Float
-            Filtering lengthscale.
-        n_ts, n_xs, n_ys : integers
-            Number of points to find observers at in (t,x,y) dimensions.
-        initial_guess (DEPRECATED): list of floats.
-            An initial guess for U. Much simpler and still robust to just use 
-            the micro velocity, u, at a given point at the initial guess.
-    
-        Returns
-        -------
-        list of coordinates, Us, and minimization errors.
-    
+        vars : list of strings 
+            strings have to be in to prim_vars keys
+        point : list of floats
+            ordered coordinates: t,x,y
+        Return
+        ------
+        list of floats corresponding to string of vars
+
+        Notes
+        -----
+        Interpolation gives errors when applied to boundary  
         """
-        u, n = self.interpolate_u_n_point(point)
-        initial_guess_vx_vy = [u[1]/u[0], u[2]/u[0]]
-        try:
-            sol = minimize(residual,x0=guess_vx_vy,args=(point,L),bounds=((-0.7,0.7),(-0.7,0.7)),tol=1e-6)#,method='CG')
-            # Large error in root-find
-            if (sol.fun > 1e-5):
-                print("Warning! Residual is large: ",sol.fun)
-        except:
-            print("Failed for ", coordinate)
-        finally:
-            pass
-        return sol
+        res = []
+        for var_name in vars:
+            try:
+                res.append( interpn(self.domain_vars["points"], self.prim_vars[var_name],point, method = self.interp_method)[0])
+            except KeyError:
+                print(f"{var_name} does not belong to the primitive variables of the micromodel!")    
+        return res
+    
+    def get_interpol_aux(self, vars, point): 
+        """
+        Returns the interpolated variable at the point
+
+        Parameters
+        ----------
+        vars : list of strings 
+            strings have to be in to aux_vars keys
+        point : list of floats
+            ordered coordinates: t,x,y
+        Return
+        ------
+        list of floats corresponding to string of vars
+
+        Notes
+        -----
+        Interpolation gives errors when applied to boundary  
+        """
+
+        res = []
+        for var_name in vars:
+            try:
+                res.append( interpn(self.domain_vars["points"], self.aux_vars[var_name],point, method = self.interp_method)[0])
+            except KeyError:
+                print(f"{var_name} does not belong to the auxiliary variables of the micromodel!")
+        return res
     
 
-    def interpolate_var(self, point, var_str):
-        return interpn(self.points,self.vars[var_str],point)[0]
+    def get_interpol_struct(self, var, point): 
+        """
+        Returns the interpolated structure at the point
+
+        Parameters
+        ----------
+        vars : str corresponding to one of the structures
+            
+        point : list of floats
+            ordered coordinates: t,x,y
+
+        Return
+        ------
+        Array with the interpolated values of the var structure
+            Empty list if var is not a structure in the micromodel
+
+        Notes
+        -----
+        Interpolation gives errors when applied to boundary  
+        """
+        res = []
+        if var == "bar_vel":
+            res = np.zeros(len(self.structures[var][:,0,0,0]))
+            for a in range(len(self.structures[var][:,0,0,0])):
+                res[a] = interpn(self.domain_vars["points"], self.structures[var][a,:,:,:],point, method = self.interp_method)[0]
+        elif var == "SET":   
+            res = np.zeros((len(self.structures[var][:,0,0,0,0]),len(self.structures[var][0,:,0,0,0])))
+            for a in range(len(self.structures[var][:,0,0,0,0])):
+                for b in range(len(self.structures[var][0,:,0,0,0])):
+                    res[a,b] = interpn(self.domain_vars["points"],self.structures[var][a,b,:,:,:],point, method = self.interp_method)[0]                   
+        elif var == "Faraday":
+            res = np.zeros((len(self.structures[var][:,0,0,0,0]),len(self.structures[var][0,:,0,0,0])))
+            for a in range(len(self.structures[var][:,0,0,0,0])):
+                for b in range(len(self.structures[var][0,:,0,0,0])):
+                    res[a,b]= interpn(self.domain_vars["points"],self.structures[var][a,b,:,:,:],point, method = self.interp_method)[0]
+        else:
+            print(f"{var} does not belong to the structures in the Micromodel")
+        return res
 
 
+if __name__ == '__main__':
+    FileReader = METHOD_HDF5('./Data/test_res100/')
+    MicroModel = IdealMHD_2D()
+    FileReader.read_in_data(MicroModel) 
+    MicroModel.setup_structures()
 
+    # res = MicroModel.get_interpol_prim(["vz","vx","Bx"],[0.5, 0.3,0.2])
+    # print(type(res),"\n", res)
+    # res = MicroModel.get_interpol_aux(["b0","bx"],[0.5, 0.3,0.2])
+    # print(type(res),"\n", res)
 
+    # res = MicroModel.get_interpol_struct(["SET"],[2.5, 0.3,0.2])
+    # print(res.shape, res[0])
 
-
-
-
-
-
-
-
-
-
-
-
-
+    # print( MicroModel.get_domain_strs() )
 
